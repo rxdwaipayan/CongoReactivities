3bac02f0454a70f80d95d7694208ef900633a168
Dwaipayan Bhattacharya <rxdwaipayan@gmail.com>
2025-06-17 11:28:24 +0530

    End of React basic walking skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
+// Configure the HTTP request pipeline.
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
